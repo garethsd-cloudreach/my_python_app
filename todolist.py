6af4d43eb3c4759a935ba06d7c8baea3a8b1cdde
Gareth Scott-Dodd <garethscott-dodd@gareths-mbp.home>
2021-11-16 19:05:34 +0000

    python app day one

--- todolist.py
+++ todolist.py
@@ -3,21 +3,31 @@ import json
 
 app = Flask(__name__)
 
-todolist = {
-    "1" : "washing up",
-    "2" : "shopping",
-    "3" : "studying",
-    "4" : "laundry"
+todo_db = {
+    "1": { 'name': 'laundry', 'status': 'to do'},
+    "2": { 'name': 'windows', 'status': 'to do'},
+    "3": { 'name': 'wash car', 'status': 'to do'}
 }
 
-@app.route("/")
+@app.route('/')
 def todo():
-    return todolist
+    return todo_db
 
 #READ
 @app.route("/todo/<todo_id>")
 def get_todo(todo_id):
-    return json.dumps(todolist[todo_id])
+    return json.dumps(todo_db[todo_id])
+
+#CREATE
+
+@app.route('/todo/add' , methods=['POST'])
+def add_todo():
+    req_data = request.get_json()
+    job = req_data['todo']
+
+    new_job = { "4" : todo }
+    todo_db.update(new_job)
+    return "Added to To Do List"
 
 @app.route("/first")
 def world():
@@ -26,3 +36,6 @@ def world():
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1')
+
+#New To Do
+# { "todo" : { "name" : "to do", "status": "to do"}}
