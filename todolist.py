5340b1f6aa06837db207254f5d779b86bf467644
Gareth Scott-Dodd <garethscott-dodd@gareths-mbp.home>
2021-11-18 17:11:21 +0000

    add - update

--- todolist.py
+++ todolist.py
@@ -20,7 +20,7 @@ def get_todo(todo_id):
 
 #CREATE
 
-@app.route('/todo/add' , methods=['POST'])
+@app.route('/add' , methods=['POST'])
 def add_todo():
     req_data = request.get_json()
     job = req_data['todo']
@@ -28,6 +28,28 @@ def add_todo():
     new_job = { "4" : todo }
     todo_db.update(new_job)
     return "Added to To Do List"
+
+#UPDATE
+
+@app.route('/update' , methods=['PUT'])
+def update_todo():
+    req_data = request.get_json()
+    job = req_data['todo']
+
+    update_job = {"todo": {"task" : "laundry", "progress" : "not started"}}
+    todo_db.update(update_job)
+    return "Updated to do list"
+
+#DELETE
+
+@app.route('/delete' , methods=['DELETE'])
+def delete_todo():
+    req_data = request.get_json()
+    job = req_data['todo']
+
+    delete_todo = {"todo": {"task" : "windows", "progress" : "not started"}}
+    todo_db.update(delete_todo)
+    return "Job Deleted"
 
 @app.route("/first")
 def world():
